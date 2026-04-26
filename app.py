ac584dbddf14f8f192ebb657b80cdb9435066bbf
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-04-26 14:21:15 +0300

    sdfsd

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, Response
 from main import handle
+import html
+import os
 
 app = Flask(__name__)
 
@@ -7,18 +9,24 @@ app = Flask(__name__)
 def whatsapp():
     incoming_msg = request.form.get("Body")
 
-    # 👉 הבוט שלך
+    print("📩 INCOMING:", incoming_msg, flush=True)
+
     reply = handle(incoming_msg)
 
-    return Response(
-    f"""<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-    <Message>{reply}</Message>
-</Response>""",
-    mimetype="application/xml"
-)
+    if not reply:
+        reply = "לא הבנתי 🤔"
 
-import os
+    print("📤 REPLY:", reply, flush=True)
+
+    safe_reply = html.escape(reply)
+
+    twiml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Message>{safe_reply}</Message>
+</Response>"""
+
+    return Response(twiml, mimetype="text/xml")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
